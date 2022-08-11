971c5bf8f05c00d2d513f19f38dbed35a76e9ce6
eparisot <parisot.eric.mail@gmail.com>
2022-08-11 20:54:59 +0200

    need improvment on replace

--- srcs/elf_tools.c
+++ srcs/elf_tools.c
@@ -17,10 +17,10 @@ static int replace_addr(t_env *env, unsigned int needle, unsigned int replace, i
 			if (found) {	
 				if (offset == 1) // take current position in account
 					replace -= (i * 8 + j); // replace should be negative
-				*(unsigned int *)(&((unsigned char *)(&((long unsigned int *)env->payload_content)[i]))[j]) = replace;
+				*((unsigned int *)(&((unsigned char *)(&((long unsigned int *)env->payload_content)[i]))[j])) = replace;
 				// replace "leave ret" : c9c3 by NOP to slide to jmp with replaced address
-				*((unsigned char *)(&((unsigned char *)(&((long unsigned int *)env->payload_content)[i]))[j]) - 2) = 0x90;
 				*((unsigned char *)(&((unsigned char *)(&((long unsigned int *)env->payload_content)[i]))[j]) - 3) = 0x90;
+				*((unsigned char *)(&((unsigned char *)(&((long unsigned int *)env->payload_content)[i]))[j]) - 2) = 0x90;
 				break;
 			}
 		}
