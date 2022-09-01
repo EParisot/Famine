00b9d5903801390b3e7455b66e90dd07846253d9
eparisot <parisot.eric.mail@gmail.com>
2022-09-01 16:31:55 +0200

    improve tests

--- tests.sh
+++ tests.sh
@@ -7,6 +7,16 @@ YELLOW='\033[0;33m'
 BLUE='\033[0;34m'
 NC='\033[0m' # No Color
 
+SIGN='Famine version 1.0 (c)oded by eparisot'
+
+function check() {
+	if strings $1 | grep "$SIGN"; then
+		printf "${GREEN}[OK]${NC}\n"
+	else
+		printf "${RED}[FAIL]${NC}\n"
+	fi
+}
+
 make re
 
 printf "\n${RED}Famine simple tests: ${NC}\n"
@@ -16,58 +26,58 @@ printf "\n${YELLOW}* infected hello infects fresh hello: ${NC}\n"
 gcc ressources/sample.c -o /tmp/test2/hello
 /tmp/test/hello
 /tmp/test2/hello
-strings /tmp/test2/hello | grep "eparisot"
+check /tmp/test2/hello
 printf "\n${YELLOW}* infected hello2 infects fresh hello2: ${NC}\n"
 gcc ressources/sample2.c -o /tmp/test2/hello2
 /tmp/test/hello2
 /tmp/test2/hello2
-strings /tmp/test2/hello2 | grep "eparisot"
+check /tmp/test2/hello2
 printf "\n${YELLOW}* infected ls infects fresh ls: ${NC}\n"
 cp /bin/ls /tmp/test2/ls
 /tmp/test/ls
 /tmp/test2/ls
-strings /tmp/test2/ls | grep "eparisot"
+check /tmp/test2/ls
 printf "\n${YELLOW}* infected date infects fresh date: ${NC}\n"
 cp /bin/date /tmp/test2/date
 /tmp/test/date
 /tmp/test2/date
-strings /tmp/test2/date | grep "eparisot"
+check /tmp/test2/date
 printf "\n${YELLOW}* infected grep infects fresh grep: ${NC}\n"
 cp /bin/grep /tmp/test2/grep
 /tmp/test/grep
 /tmp/test2/grep
-strings /tmp/test2/grep | grep "eparisot"
+check /tmp/test2/grep
 
 printf "\n${YELLOW}* infected ls infects fresh hello: ${NC}\n"
 gcc ressources/sample.c -o /tmp/test2/hello
 /tmp/test/ls
 /tmp/test2/hello
-strings /tmp/test2/hello | grep "eparisot"
+check /tmp/test2/hello
 printf "\n${YELLOW}* infected hello infects fresh hello2: ${NC}\n"
 gcc ressources/sample2.c -o /tmp/test2/hello2
 /tmp/test/hello
 /tmp/test2/hello2
-strings /tmp/test2/hello2 | grep "eparisot"
+check /tmp/test2/hello2
 printf "\n${YELLOW}* infected hello infects fresh ls: ${NC}\n"
 cp /bin/ls /tmp/test2/ls
 /tmp/test/hello
 /tmp/test2/ls
-strings /tmp/test2/ls | grep "eparisot"
+check /tmp/test2/ls
 printf "\n${YELLOW}* infected hello2 infects fresh ls: ${NC}\n"
 cp /bin/ls /tmp/test2/ls
 /tmp/test/hello2
 /tmp/test2/ls -la /tmp
-strings /tmp/test2/ls | grep "eparisot"
+check /tmp/test2/ls
 printf "\n${YELLOW}* infected hello infects fresh date: ${NC}\n"
 cp /bin/date /tmp/test2/date
 /tmp/test/hello
 /tmp/test2/date
-strings /tmp/test2/date | grep "eparisot"
+check /tmp/test2/date
 printf "\n${YELLOW}* infected hello infects fresh grep: ${NC}\n"
 cp /bin/grep /tmp/test2/grep
 /tmp/test/hello
 cat ressources/sample.c | /tmp/test2/grep 'int'
-strings /tmp/test2/hello | grep "eparisot"
+check /tmp/test2/hello
 
 echo ""
 
@@ -94,7 +104,7 @@ printf "${RED}Running Famine...${NC}"
 printf "${RED}Done.${NC}\n"
 for f in /tmp/test/*
 do 
-	if [[ "$(strings $f | grep eparisot)" ]]; then
+	if [[ '$(strings $f | grep $SIGN)' ]]; then
 		printf "${GREEN}.${NC}"
 	else
 		printf "${RED}.${NC}"
